dde1a73e8946444d4f3c2b8cfbb035cbfb81920f
Kaio Piola <kaio.piola@hotmail.com>
2025-12-18 09:12:13 -0300

    1.1.0 to brew formula

--- Formula/curlser.rb
+++ Formula/curlser.rb
@@ -5,8 +5,8 @@
 class Curlser < Formula
   desc "Colorful HTTP client for terminal with automatic formatting"
   homepage "https://github.com/kaiopiola/curlser"
-  url "https://github.com/kaiopiola/curlser/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "3b7b79989301988a4ae9ed421af20218511f1963faed2035f826590cb5f3bc96"
+  url "https://github.com/kaiopiola/curlser/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "11cac5bfb17a616410343d9a4beda9d8af94ca68eb2357c509d969eeba7abe10"
   license "MIT"
 
   depends_on "curl"
